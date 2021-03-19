3e662e4810a247b972b18d9b87140634c06b314b
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2021-03-19 21:14:35 +0100

    file input box

--- resources/views/home-control/index.blade.php
+++ resources/views/home-control/index.blade.php
@@ -46,7 +46,7 @@
               To: "transform opacity-0 scale-95"
           -->
             <div x-show="open" @click.away="open = false"
-                class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10"
                 role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                 <div class="py-1" role="none">
                     <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
@@ -66,7 +66,7 @@
             </div>
         </div>
     </nav>
-    <main class="px-12">
+    <main class="flex justify-center">
         <div id="content" class="flex flex-col space-y-4">
             <div class="flex flex-col mt-8">
                 <label for="text" class="block text-sm font-medium text-gray-700">Text to speech</label>
@@ -77,7 +77,7 @@
                     <div class="absolute inset-y-0 right-0 flex items-center">
                         <div class="inline-block">
                             <button type="button"
-                                class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-r-md  bg-blue-500 hover:bg-blue-600 hover:shadow-lg">Danger</button>
+                                class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-r-md  bg-blue-500 hover:bg-blue-600 hover:shadow-lg">Send</button>
                         </div>
                     </div>
                 </div>
@@ -98,8 +98,23 @@
                     </ul>
                 </div>
             </div>
-            <div id="text-to-speech" class="bg-red-300">
-                hey
+            <div id="upload-file" class="">
+                <label for="text" class="block text-sm font-medium text-gray-700">Display on TV</label>
+                <div class="flex flex-col items-center flex-grow mb-3">
+                    <label
+                        class="w-full flex flex-col items-center px-4 py-6 bg-blue-400 uppercase rounded-lg cursor-pointer hover:bg-blue hover:text-white">
+                        <span class="mt-2 text-base leading-normal">Select a file</span>
+                        <input type='file' class="hidden" />
+                    </label>
+                </div>
+                <div id="recent-images" class="grid grid-cols-3 gap-4 mt-4">
+                    <div class="bg-blue-300 h-20 w-20"></div>
+                    <div class="bg-blue-300 h-20 w-20"></div>
+                    <div class="bg-blue-300 h-20 w-20"></div>
+                    <div class="bg-blue-300 h-20 w-20"></div>
+                    <div class="bg-blue-300 h-20 w-20"></div>
+                    <div class="bg-blue-300 h-20 w-20"></div>
+                </div>
             </div>
             <div id="text-to-speech" class="bg-green-300">
                 hey
