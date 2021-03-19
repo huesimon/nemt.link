e5306a46ce3b597cd06fd5ef976ceb0926ab3cd8
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2021-03-19 23:07:40 +0100

    Update index.blade.php

--- resources/views/home-control/index.blade.php
+++ resources/views/home-control/index.blade.php
@@ -116,9 +116,36 @@
                     <div class="bg-blue-300 h-20 w-20"></div>
                 </div>
             </div>
-            <div id="text-to-speech" class="bg-green-300">
-                hey
+            <div id="change-color" class="">
+                <label for="text" class="block text-sm font-medium text-gray-700">Change light color</label>
+                <div class="flex items-center justify-between">
+                    <select name="color" id="color">
+                        <option class="bg-red-500" value="red">Red</option>
+                        <option class="bg-blue-500" value="blue">Blue</option>
+                        <option class="bg-green-500" value="green">Green</option>
+                        <option class="bg-yellow-500" value="yellow">Yellow</option>
+                        <option class="bg-pink-500" value="pink">Pink</option>
+
+                    </select>
+                    <div class="inline-block">
+                        <button type="button"
+                            class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-r-md  bg-blue-500 hover:bg-blue-600 hover:shadow-lg">Change</button>
+                    </div>
+                </div>
             </div>
+            <div id="recent-colors" class="grid grid-cols-6 gap-4 mt-4">
+                <div class="bg-blue-300 h-8 w-8 rounded-full"></div>
+                <div class="bg-blue-300 h-8 w-8 rounded-full"></div>
+                <div class="bg-blue-300 h-8 w-8 rounded-full"></div>
+                <div class="bg-blue-300 h-8 w-8 rounded-full"></div>
+                <div class="bg-blue-300 h-8 w-8 rounded-full"></div>
+                <div class="bg-blue-300 h-8 w-8 rounded-full"></div>
+                <div class="bg-blue-300 h-8 w-8 rounded-full"></div>
+                <div class="bg-blue-300 h-8 w-8 rounded-full"></div>
+                <div class="bg-blue-300 h-8 w-8 rounded-full"></div>
+            </div>
+        </div>
+
         </div>
     </main>
     @livewireScripts
